2b3d34719234cbecb5a0e2d564836a0b8a5ea2de
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-03-11 18:20:12 +0330

    models registered in admin panel

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import ChatBox, Message
+
 # Register your models here.
+admin.site.register(ChatBox)
+admin.site.register(Message)
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import User, Group, GroupMember, GroupJoinRequest, GroupConnectionRequest
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Group)
+admin.site.register(GroupMember)
+admin.site.register(GroupJoinRequest)
+admin.site.register(GroupConnectionRequest)
