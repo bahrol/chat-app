4f684b8d6d9e85515b0dea9f5c6800cb4af8b26a
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-03-11 13:47:52 +0330

    group url and views implemented

--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('auth/signup/', views.SignupView.as_view(), name='signup'),
     path('auth/login/', views.LoginView.as_view(), name='login'),
+    path('groups/', views.GroupView.as_view(), name='groups'),
+    path('groups/my/', views.MyGroupView.as_view(), name='my_group'),
 ]
--- user/views.py
+++ user/views.py
@@ -4,8 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_jwt.serializers import JSONWebTokenSerializer
 
-from .models import User
-from .serializers import UserSerializer
+from .models import User, Group, GroupMember
+from .serializers import UserSerializer, GroupSerializer
 
 
 # Create your views here.
@@ -46,3 +46,54 @@ class LoginView(APIView):
         else:
             response_data = {"error": {"enMessage": "Bad request!"}}
             return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GroupView(APIView):
+    serializer_class = GroupSerializer
+    query_set = Group.objects.all()
+
+    def get(self, request):
+        ser = self.serializer_class(data=self.query_set, many=True)
+        response_data = {"groups": ser.data}
+        return Response(response_data, status=status.HTTP_200_OK)
+
+    def post(self, request):    # Create a group
+        ser = self.serializer_class(data=request.data)
+        if not ser.is_valid():
+            response_data = {"error": {"enMessage": "Bad request!"}}
+            return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+        response_data = {"group": {"id": ser.data.get('id')}, "message": "successful"}
+        GroupMember.objects.create(group=ser.data.get('id'), user=request.user, role=GroupMember.OWNER)
+        return Response(response_data, status=status.HTTP_200_OK)
+
+
+class MyGroupView(APIView):
+    serializer_class = GroupSerializer
+
+    def get(self, request):
+        user = request.user
+
+        try:
+            gm = GroupMember.objects.get(user=user)
+        except GroupMember.DoesNotExist:    # User is not in any group
+            response_data = {"error": {"enMessage": "Bad request!"}}
+            return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+
+        group = gm.group
+        group_member_queryset = GroupMember.objects.filter(group=group)
+        members = []
+        for group_member in group_member_queryset:
+            member = {
+                "id": group_member.user.id,
+                "name": group_member.user.name,
+                "email": group_member.user.email,
+                "rule": group_member.role,
+            }
+            members.append(member)
+
+        response_data = {
+            "name": group.name,
+            "description": group.description,
+            "members": members
+        }
+        return Response(response_data, status=status.HTTP_200_OK)
