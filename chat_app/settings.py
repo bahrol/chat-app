32822d8c06e7c2f308e31c858e05ed655cc43c68
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-03-11 11:32:05 +0330

    chat and user app created

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party apps
+    'rest_framework',
+    'rest_framework_jwt',
+    # local apps
+    'user',
+    'chat',
 ]
 
 MIDDLEWARE = [
